7a3f7cad9862942408caef6e5390139b9d4de82f
hrdkmishra <hardikmishra0509@gmial.com>
2023-06-27 20:24:49 +0530

    added db with login , sign and logout

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
-from flask import Flask, render_template, request
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+import bcrypt
+from flask import Flask, render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL
 from forms import SignupForm, LoginForm
 
@@ -15,6 +13,7 @@ app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'flask'
 mysql = MySQL(app)
+
 
 # with app.app_context():
 #     try:
@@ -40,16 +39,20 @@ def login():
         email = form.email.data
         password = form.password.data
 
-        # Perform validation and authentication logic here
-        if email == 'example@example.com' and password == 'password':
-            # Successful login
-            return 'Login successful!'
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * FROM users WHERE email = %s", [email])
+        user = cur.fetchone()
+        # print(user) = (1, 'admin@website.com', 'admin', '', '$2b$12$9.xkOBOu8gvYWDrWzcj56er/j029XsGBfWzvJLQIJXiajic53LKaK')
+        # the data user return is a tuple
+        cur.close()
+
+        if user and bcrypt.checkpw(password.encode('utf-8'), user[4].encode('utf-8')):
+            session['user'] = user, {'first_name': user[2], 'last_name': user[3]}
+            return redirect(url_for('home'))
         else:
-            # Invalid credentials
-            return 'Invalid email or password'
+            return 'Login failed!'
 
     return render_template('login.html', form=form)
-
 
 
 @app.route('/signup', methods=['GET', 'POST'])
@@ -58,19 +61,31 @@ def signup():
 
     if form.validate_on_submit():
         email = form.email.data
+        first_name = form.first_name.data
+        last_name = form.last_name.data
         password = form.password.data
+        confirm_password = form.confirm_password.data
+
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
         cur = mysql.connection.cursor()
         cur.execute(
-            "INSERT INTO users (email, first_name, last_name, password) VALUES (%s, %s, %s, %s, %s)",
-            (email, password)
+            "INSERT INTO users (email, first_name, last_name, password) VALUES (%s, %s, %s, %s)",
+            (email, first_name, last_name, hashed_password)
         )
         mysql.connection.commit()
         cur.close()
 
-        return 'Signup successful!'
+        session['user'] = {'email': email, 'first_name': first_name, 'last_name': last_name}
+        return redirect(url_for('home'))
 
     return render_template('signup.html', form=form)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('user', None)  # Remove user information from the session
+    return redirect(url_for('home'))
 
 
 @app.route('/employer')
--- forms.py
+++ forms.py
@@ -1,15 +1,14 @@
-
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email, EqualTo
+from wtforms.validators import DataRequired, Email, EqualTo, InputRequired, Length
 
 
 class SignupForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     first_name = StringField('First Name', validators=[DataRequired()])
     last_name = StringField('Last Name')
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    password = PasswordField('Password', validators=[InputRequired(), DataRequired(), Length(min=8, max=30)])
+    confirm_password = PasswordField('Confirm Password', validators=[InputRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
 
